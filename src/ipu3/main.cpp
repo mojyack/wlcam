908ed7c74e0dea57152079260e2bfaf52bfb251f
mojyack <mojyack@gmail.com>
2024-06-22 18:44:03 +0900

    ipu3: add comments

--- src/ipu3/main.cpp
+++ src/ipu3/main.cpp
@@ -32,6 +32,7 @@ class IPU3WindowCallbacks : public WindowCallbacks {
 auto run(const int argc, const char* const argv[]) -> bool {
     unwrap_ob(args, ipu3::parse_args(argc, argv));
 
+    // find devices
     unwrap_ob(node_map, dev::enumerate());
     unwrap_ob_mut(cio2_device, parse_device(args.cio2_devnode, node_map));
     assert_b(cio2_device.disable_all_links());
@@ -45,6 +46,7 @@ auto run(const int argc, const char* const argv[]) -> bool {
     auto cio2_0 = ipu3::cio2::CIO2Device(&cio2_device);
     assert_b(cio2_0.init(args.cio2_entity));
 
+    // configure pipeline
     constexpr auto imgu_input_format = v4l2_fourcc('i', 'p', '3', 'b');
     constexpr auto num_buffers       = 3;
     constexpr auto outbuf_mp         = V4L2_BUF_TYPE_VIDEO_OUTPUT_MPLANE;
@@ -107,6 +109,7 @@ auto run(const int argc, const char* const argv[]) -> bool {
     assert_b(v4l2::request_buffers(cio2_output_fd, capbuf_mp, V4L2_MEMORY_MMAP, 0));
     assert_b(v4l2::request_buffers(cio2_output_fd, capbuf_mp, V4L2_MEMORY_DMABUF, num_buffers));
 
+    // start stream
     assert_b(v4l2::start_stream(cio2_output_fd, capbuf_mp));
     assert_b(v4l2::start_stream(imgu_output_fd, capbuf_mp));
     assert_b(v4l2::start_stream(imgu_vf_fd, capbuf_mp));
@@ -114,6 +117,7 @@ auto run(const int argc, const char* const argv[]) -> bool {
     assert_b(v4l2::start_stream(imgu_stat_fd, capbuf_meta));
     assert_b(v4l2::start_stream(imgu_input_fd, outbuf_mp));
 
+    // get buffers
     for(auto i = 0; i < num_buffers; i += 1) {
         assert_b(v4l2::queue_buffer_mp(cio2_output_fd, capbuf_mp, V4L2_MEMORY_DMABUF, i, &cio2_output_buffers[i], 1));
     }
@@ -144,6 +148,7 @@ auto run(const int argc, const char* const argv[]) -> bool {
         init_params_buffer(*params_mmap_ptrs[i], pipeline_config, bds_grid);
     }
 
+    // prepare gui
     auto context = Context();
 
     auto       window_callbacks = std::shared_ptr<IPU3WindowCallbacks>(new IPU3WindowCallbacks(context));
@@ -160,11 +165,10 @@ auto run(const int argc, const char* const argv[]) -> bool {
 
     init_yuv420sp_shader();
 
-    auto finish_camera_thread = false;
-    auto camera_thread        = std::thread([&]() -> bool {
+    auto running       = false;
+    auto camera_thread = std::thread([&]() -> bool {
         auto       window_context = wlwindow->fork_context();
         auto       file_manager   = FileManager(args.savedir);
-        auto       event_fifo     = args.event_fifo != nullptr ? RemoteServer(args.event_fifo) : RemoteServer();
         const auto output_width   = imgu_output_fmt.fmt.pix_mp.width;
         const auto output_height  = imgu_output_fmt.fmt.pix_mp.height;
         const auto output_stride  = imgu_output_fmt.fmt.pix_mp.plane_fmt[0].bytesperline;
@@ -174,61 +178,64 @@ auto run(const int argc, const char* const argv[]) -> bool {
         auto       ubuf           = std::vector<std::byte>(output_height / 4 * output_width);
         auto       vbuf           = std::vector<std::byte>(output_height / 4 * output_width);
 
-        if(event_fifo) {
-            event_fifo.send_event(RemoteEvents::Hello{});
-            event_fifo.send_event(RemoteEvents::Raw{build_string("ipu3 sensor ", cio2_0.sensor.dev_node)});
-            if(cio2_0.sensor.lens) {
-                event_fifo.send_event(RemoteEvents::Raw{build_string("ipu3 lens ", cio2_0.sensor.lens->dev_node)});
-            }
+        print("ipu3 sensor ", cio2_0.sensor.dev_node);
+        if(cio2_0.sensor.lens) {
+            print("ipu3 lens ", cio2_0.sensor.lens->dev_node);
         }
 
-        while(!finish_camera_thread) {
-            unwrap_ob(i, v4l2::dequeue_buffer_mp(cio2_output_fd, capbuf_mp, V4L2_MEMORY_DMABUF));
+    loop:
+        if(!running) {
+            return true;
+        }
+        // get raw image
+        unwrap_ob(i, v4l2::dequeue_buffer_mp(cio2_output_fd, capbuf_mp, V4L2_MEMORY_DMABUF));
 
-            assert_b(v4l2::queue_buffer_mp(imgu_output_fd, capbuf_mp, V4L2_MEMORY_DMABUF, i, &imgu_output_buffers[i], 1));
-            assert_b(v4l2::queue_buffer_mp(imgu_vf_fd, capbuf_mp, V4L2_MEMORY_DMABUF, i, &imgu_vf_buffers[i], 1));
-            assert_b(v4l2::queue_buffer(imgu_param_fd, outbuf_meta, i));
-            assert_b(v4l2::queue_buffer(imgu_stat_fd, capbuf_meta, i));
-            assert_b(v4l2::queue_buffer_mp(imgu_input_fd, outbuf_mp, V4L2_MEMORY_DMABUF, i, &cio2_output_buffers[i], 1));
+        // start processing
+        assert_b(v4l2::queue_buffer_mp(imgu_output_fd, capbuf_mp, V4L2_MEMORY_DMABUF, i, &imgu_output_buffers[i], 1));
+        assert_b(v4l2::queue_buffer_mp(imgu_vf_fd, capbuf_mp, V4L2_MEMORY_DMABUF, i, &imgu_vf_buffers[i], 1));
+        assert_b(v4l2::queue_buffer(imgu_param_fd, outbuf_meta, i));
+        assert_b(v4l2::queue_buffer(imgu_stat_fd, capbuf_meta, i));
+        assert_b(v4l2::queue_buffer_mp(imgu_input_fd, outbuf_mp, V4L2_MEMORY_DMABUF, i, &cio2_output_buffers[i], 1));
 
-            assert_b(v4l2::dequeue_buffer_mp(imgu_output_fd, capbuf_mp, V4L2_MEMORY_DMABUF));
-            assert_b(v4l2::dequeue_buffer_mp(imgu_vf_fd, capbuf_mp, V4L2_MEMORY_DMABUF));
-            assert_b(v4l2::dequeue_buffer(imgu_param_fd, outbuf_meta));
-            assert_b(v4l2::dequeue_buffer(imgu_stat_fd, capbuf_meta));
-            assert_b(v4l2::dequeue_buffer_mp(imgu_input_fd, outbuf_mp, V4L2_MEMORY_DMABUF));
+        // get processed image
+        assert_b(v4l2::dequeue_buffer_mp(imgu_output_fd, capbuf_mp, V4L2_MEMORY_DMABUF));
+        assert_b(v4l2::dequeue_buffer_mp(imgu_vf_fd, capbuf_mp, V4L2_MEMORY_DMABUF));
+        assert_b(v4l2::dequeue_buffer(imgu_param_fd, outbuf_meta));
+        assert_b(v4l2::dequeue_buffer(imgu_stat_fd, capbuf_meta));
+        assert_b(v4l2::dequeue_buffer_mp(imgu_input_fd, outbuf_mp, V4L2_MEMORY_DMABUF));
 
-            auto       frame      = std::shared_ptr<Frame>(new YUV420SPFrame(vf_width, vf_height, vf_stride));
-            const auto byte_array = Frame::ByteArray{static_cast<std::byte*>(vf_mmap_ptrs[i]), imgu_vf_buffers[i].length};
-            assert_b(frame->load_texture(byte_array));
+        // load texutre
+        auto       frame      = std::shared_ptr<Frame>(new YUV420SPFrame(vf_width, vf_height, vf_stride));
+        const auto byte_array = Frame::ByteArray{static_cast<std::byte*>(vf_mmap_ptrs[i]), imgu_vf_buffers[i].length};
+        assert_b(frame->load_texture(byte_array));
 
-            // proc commands while flushing texture
-            switch(std::exchange(context.camera_command, Command::None)) {
-            case Command::TakePhoto: {
-                const auto path = file_manager.get_next_path().string() + ".jpg";
+        // process commands while flushing texture
+        switch(std::exchange(context.camera_command, Command::None)) {
+        case Command::TakePhoto: {
+            const auto path = file_manager.get_next_path().string() + ".jpg";
 
-                const auto byte_array = Frame::ByteArray{static_cast<std::byte*>(output_mmap_ptrs[i]), imgu_output_buffers[i].length};
-                auto       frame      = YUV420SPFrame(output_width, output_height, output_stride);
-                assert_b(frame.save_to_jpeg(byte_array, path));
+            const auto byte_array = Frame::ByteArray{static_cast<std::byte*>(output_mmap_ptrs[i]), imgu_output_buffers[i].length};
+            auto       frame      = YUV420SPFrame(output_width, output_height, output_stride);
+            assert_b(frame.save_to_jpeg(byte_array, path));
 
-                context.ui_command = Command::TakePhotoDone;
-            } break;
-            default:
-                break;
-            }
-
-            window_context.flush();
-            context.frame = frame;
-
-            assert_b(v4l2::queue_buffer_mp(cio2_output_fd, capbuf_mp, V4L2_MEMORY_DMABUF, i, &cio2_output_buffers[i], 1));
+            context.ui_command = Command::TakePhotoDone;
+        } break;
+        default:
+            break;
         }
 
-        event_fifo.send_event(RemoteEvents::Bye{});
-        return true;
+        // update displayed image
+        window_context.flush();
+        context.frame = frame;
+
+        // release buffer to system
+        assert_b(v4l2::queue_buffer_mp(cio2_output_fd, capbuf_mp, V4L2_MEMORY_DMABUF, i, &cio2_output_buffers[i], 1));
+        goto loop;
     });
 
     app.run();
 
-    finish_camera_thread = true;
+    running = true;
     camera_thread.join();
     std::quick_exit(0);
 
